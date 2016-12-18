bf6c8e581f675fbd7fae4864cfb9666bf8dd09a2
amidart <fromamid@gmail.com>
2016-12-18 21:51:00 +0700

    version 1.4.3: updated position due to google layout change

--- js/add_button.js
+++ js/add_button.js
@@ -1,13 +1,14 @@
 window.onload = function () {
   var button = document.createElement("div");
-  button.style.cssText = 'display:inline-block; vertical-align: top';
-  button.innerHTML = '<button value="Search" style="width:60px;height:40px;border-radius:2px;background-color:#ffdb4c;border:solid 2px #fc0;cursor:pointer"><span style="color: red">Y</span>andex</button>';
+  button.style.cssText = 'display:inline-block; vertical-align: top;margin-left:2px';
+  button.innerHTML = '<button value="Search" style="width:60px;height:44px;border-radius:2px;background-color:#ffdb4c;border:solid 2px #fc0;cursor:pointer"><span style="color: red">Y</span>andex</button>';
   button.onclick = function() {
     var subdomain = '';
     if (document.querySelector('.hdtb-msel').textContent === 'Images') subdomain = 'images.';
     var url = 'http://' + subdomain + 'yandex.ru/yandsearch?text=' + encodeURIComponent(document.querySelector('#lst-ib').value);
     var win = window.open(url, '_blank');
   };
-  document.querySelector('#sbds').appendChild(button);
-  document.querySelector('.lsbb.kpbb').style.display = 'inline-block';
+  document.querySelector('#sbtc .sbibtd').appendChild(button);
+  document.querySelector('#sfdiv').style.display = 'inline-block';
+  document.querySelector('#sfdiv').style.width = '100%';
 };
