f520b300041598e522af347e327894dd6a0efdbe
Dmitry Artamoshkin <fromamid@gmail.com>
2018-11-21 23:55:10 +0300

    v1.4.5: fixed position and query selectors

--- js/add_button.js
+++ js/add_button.js
@@ -1,14 +1,15 @@
 window.onload = function () {
   var button = document.createElement("div");
-  button.style.cssText = 'display:inline-block; vertical-align: top;margin-left:2px';
+  button.style.cssText = 'position:absolute;right:-65px;top:0;';
   button.innerHTML = '<button value="Search" style="width:60px;height:44px;border-radius:2px;background-color:#ffdb4c;border:solid 2px #fc0;cursor:pointer"><span style="color: red">Y</span>andex</button>';
   button.onclick = function() {
     var subdomain = '';
     if (document.querySelector('.hdtb-msel').textContent === 'Images') subdomain = 'images.';
-    var url = 'http://' + subdomain + 'yandex.ru/yandsearch?text=' + encodeURIComponent(document.querySelector('#lst-ib').value);
+    var url = 'http://' + subdomain + 'yandex.ru/yandsearch?text=' + encodeURIComponent(document.querySelector('input[name=q]').value);
     var win = window.open(url, '_blank');
   };
-  document.querySelector('#tsf').appendChild(button);
-  document.querySelector('#tsf').style.maxWidth = 'none';
-  document.querySelector('#tsf > div[jsmodel]').style.display = 'inline-block';
+  var target = document.querySelector('#sbtc, [jsname=RNNXgb]');
+  if (!target) return;
+  target.appendChild(button);
+  target.style.position = 'relative';
 };
