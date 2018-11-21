567e4464b894c1650ca33ad1b63c7ba91bc6e2a4
Artem Listikov <pu6elozed@gmail.com>
2018-11-21 15:45:27 +0300

    fix work
    fixed extention, does not work click on button, based on 1.4.5 version from chrome webstore

--- js/add_button.js
+++ js/add_button.js
@@ -1,14 +1,15 @@
 window.onload = function () {
   var button = document.createElement("div");
-  button.style.cssText = 'display:inline-block; vertical-align: top;margin-left:2px';
-  button.innerHTML = '<button value="Search" style="width:60px;height:44px;border-radius:2px;background-color:#ffdb4c;border:solid 2px #fc0;cursor:pointer"><span style="color: red">Y</span>andex</button>';
+  button.style.cssText = 'position:absolute;right:-65px;top:0;';
+  button.innerHTML = '<button type="button" value="Search" style="width:60px;height:44px;border-radius:2px;background-color:#ffdb4c;border:solid 2px #fc0;cursor:pointer"><span style="color: red">Y</span>andex</button>';
   button.onclick = function() {
     var subdomain = '';
     if (document.querySelector('.hdtb-msel').textContent === 'Images') subdomain = 'images.';
-    var url = 'http://' + subdomain + 'yandex.ru/yandsearch?text=' + encodeURIComponent(document.querySelector('#lst-ib').value);
+    var url = 'http://' + subdomain + 'yandex.ru/yandsearch?text=' + encodeURIComponent(document.querySelector('input[name=q]').value);
     var win = window.open(url, '_blank');
   };
-  document.querySelector('#sbtc .sbibtd').appendChild(button);
-  document.querySelector('#sfdiv').style.display = 'inline-block';
-  document.querySelector('#sfdiv').style.width = '100%';
+  var target = document.querySelector('#sbtc, [jsname=RNNXgb]');
+  if (!target) return;
+  target.appendChild(button);
+  target.style.position = 'relative';
 };
