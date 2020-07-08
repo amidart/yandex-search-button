7f96527ee9063b56ae92a61d4b307cd73d3ec877
Artem Listikov <pu6elozed@gmail.com>
2020-07-08 15:40:17 +0300

    fix work
    fix detection of image search, update yandex urls

--- js/add_button.js
+++ js/add_button.js
@@ -3,9 +3,11 @@ window.onload = function () {
   button.style.cssText = 'position:absolute;right:-65px;top:0;';
   button.innerHTML = '<button type="button" value="Search" style="width:60px;height:44px;border-radius:2px;background-color:#ffdb4c;border:solid 2px #fc0;cursor:pointer"><span style="color: red">Y</span>andex</button>';
   button.onclick = function() {
-    var subdomain = '';
-    if (document.querySelector('.hdtb-msel').textContent === 'Images') subdomain = 'images.';
-    var url = 'http://' + subdomain + 'yandex.ru/yandsearch?text=' + encodeURIComponent(document.querySelector('input[name=q]').value);
+    let partUrl = "https://yandex.ru/yandsearch";
+    if (document.querySelector('.F7UV7d')){
+      partUrl = "https://yandex.ru/images/search";
+    };
+    var url = partUrl+'?text=' + encodeURIComponent(document.querySelector('input[name=q]').value);
     var win = window.open(url, '_blank');
   };
   var target = document.querySelector('#sbtc, [jsname=RNNXgb]');
